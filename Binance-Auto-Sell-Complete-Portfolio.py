cc7b690eaad251da2342407885b5958d5b9338bb
Michael Hollender <111327102+michaelhollender@users.noreply.github.com>
2025-08-13 16:18:44 -0400

    Added project outline

---
+++ Binance-Auto-Sell-Complete-Portfolio.py
@@ -0,0 +1,17 @@
+# import needed packages
+
+# open connection to binance api
+
+# grab current crytpo portfolio
+
+# ierate through current crypto portfolio
+
+# test for crypto balances over $0.00
+
+# Return tickers
+
+# sell crypto for USDT
+
+# close connection to binance api
+
+# stop Python script
